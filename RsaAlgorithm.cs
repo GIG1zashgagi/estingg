0f42c9416d15af7a7912bb581539a82105cabd81
Илья Завадский <ilazavadskij715@gmail.com>
2026-05-12 19:14:14 +0300

    Подключаем необходимые пространства

--- RsaAlgorithm.cs
+++ RsaAlgorithm.cs
@@ -1,8 +1,7 @@
-﻿// ШАГ 1: Подключаем необходимые пространства имён
-using System;
-using System.Numerics;      // Для работы с большими числами (BigInteger)
-using System.Text;          // Для кодировки UTF-8 (поддержка русских букв)
-using System.Collections.Generic;  // Для использования списков (List)
+﻿using System;
+using System.Numerics;    
+using System.Text;         
+using System.Collections.Generic;  
 
 namespace testingg
 {
