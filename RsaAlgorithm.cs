254e289863dbcc8e3dcfa5ca3f9a8c12abfc253d
Илья Завадский <ilazavadskij715@gmail.com>
2026-05-12 19:13:03 +0300

    Добавьте файлы проекта.

---
+++ RsaAlgorithm.cs
@@ -0,0 +1,10 @@
+﻿// ШАГ 1: Подключаем необходимые пространства имён
+using System;
+using System.Numerics;      // Для работы с большими числами (BigInteger)
+using System.Text;          // Для кодировки UTF-8 (поддержка русских букв)
+using System.Collections.Generic;  // Для использования списков (List)
+
+namespace testingg
+{
+   
+}
